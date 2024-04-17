3f4dbdb66503609f9230f28407d6ccdda41e5e80
Vedant2598 <vedant.vartak500@gmail.com>
2024-04-17 23:18:15 +0530

    Initial commit

--- app/Models/admin.model.js
+++ app/Models/admin.model.js
@@ -11,16 +11,16 @@ let adminSchema=new mongoose.Schema({
     root:Boolean
 })
 
-let adminModel=mongoose.model("admin",adminSchema)
+let adminModel= mongoose.model("admin",adminSchema)
 
-//ADMIN PASSWORD : admin@2598
+// //ADMIN PASSWORD : admin@2598
 
-const adminRootUser=async()=>{
-   let data= await adminModel.findOne({username:'admin123'})
-   if(!data){
-    await adminModel.create({username:'admin25',password:'$2b$10$0EDcxg.KPdj9g4290Z6SCeUSAn7MJOzIBJc9sUSGocRJJXxgkuxKi',root:true})
-   }
-}
-adminRootUser()
+// const adminRootUser=async()=>{
+//    let data= await adminModel.findOne({username:'admin25'})
+//    if(!data){
+//     await adminModel.create({username:'admin25',password:'$2b$10$0EDcxg.KPdj9g4290Z6SCeUSAn7MJOzIBJc9sUSGocRJJXxgkuxKi',root:true})
+//    }
+// }
+// adminRootUser()
 
 module.exports=adminModel
